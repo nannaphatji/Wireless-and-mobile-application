22fd9497ecf15ae7d2288e15b6541102996804a8
nannaphatji <55127043+nannaphatji@users.noreply.github.com>
2021-02-28 23:53:24 +0700

    Add files via upload
    W6

--- main.dart
+++ main.dart
@@ -1,24 +1,28 @@
-import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  int _price = 0;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ListViews',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Kaika Book store')),
-        body: BodyLayout(),
-      ),
-    );
-  }
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart' ;
+class Todo {
+  //final String title ;
+  //final String description;
+  //Todo(this. title , this .description) ;
 }
-
+void main() {
+  runApp(MaterialApp(
+    title : 'Passing Data',
+    home: Scaffold(
+      appBar: AppBar(title: Text('Kaika Book store')),
+      body: BodyLayout(),
+    ),
+    /*home: TodosScreen(
+      todos: List .generate(
+        10,
+            (i ) => Todo(
+          'Todo $i' ,
+          'A description of what needs to be done for Todo $i',
+        ) ,
+      ) ,
+    ) ,*/
+  ) ) ;
+}
 class BodyLayout extends StatefulWidget {
   @override
   _BodyLayoutState createState() => _BodyLayoutState();
@@ -30,10 +34,61 @@ class _BodyLayoutState extends State<BodyLayout> {
     return _myListView(context);
   }
 }
+/*class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+  TodosScreen({Key key, @required this.todos}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Comics'),
+      ) ,
+      body: ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile (
+            title : Text(todos[index]. title ) ,
+// When a user taps the ListTile, navigate to the DetailScreen.
+// Notice that you're not only creating a DetailScreen, you're
+// also passing the current todo through to it .
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ) ,
+              ) ;
+            },
+          ) ;
+        },
+      ) ,
+    ) ;
+  }
+}
+class DetailScreen extends StatelessWidget {
+// Declare a field that holds the Todo.
+  final Todo todo;
 
+// In the constructor, require a Todo.
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+// Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}*/
 Widget _myListView(BuildContext context) {
-  String _textString = 'Total price: ';
+  String _textString = 'Total price: _price';
   int _price = 0;
   int temp = 0;
   return ListView(
@@ -60,6 +115,7 @@ Widget _myListView(BuildContext context) {
           temp = 320;
           _price = _price + temp;
           print(_price);
+          SelectionButton();
         },
       ),
       ListTile(
@@ -80,6 +136,7 @@ Widget _myListView(BuildContext context) {
           temp = 200;
           _price = _price + temp;
           print(_price);
+          SelectionButton();
         },
       ),
       ListTile(
@@ -100,6 +157,7 @@ Widget _myListView(BuildContext context) {
           temp = 150;
           _price = _price + temp;
           print(_price);
+          SelectionButton();
         },
       ),
       ListTile(
@@ -120,6 +178,7 @@ Widget _myListView(BuildContext context) {
           temp = 100;
           _price = _price + temp;
           print(_price);
+          SelectionButton();
         },
       ),
       ListTile(
@@ -140,6 +199,7 @@ Widget _myListView(BuildContext context) {
           temp = 125;
           _price = _price + temp;
           print(_price);
+          SelectionButton();
         },
       ),
       ListTile(
@@ -160,8 +220,70 @@ Widget _myListView(BuildContext context) {
           temp = 300;
           _price = _price + temp;
           print(_price);
+          SelectionButton();
         },
       ),
     ],
   );
+}
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: Text('Pick an option, any option!') ,
+    ) ;
+  }
+// A method that launches the SelectionScreen and awaits the result from
+// Navigator.pop.
+  _navigateAndDisplaySelection(BuildContext context) async {
+// Navigator.push returns a Future that completes after calling
+// Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    ) ;
+// After the Selection Screen returns a result , hide any previous snackbars
+// and show the new result.
+    Scaffold. of(context)
+      .. removeCurrentSnackBar()
+      .. showSnackBar(SnackBar(content: Text("$result")));
+  }
+}
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text('Pick an option') ,
+      ) ,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all (8.0) ,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, "Yes!");
+                },
+                child: Text('Yes!') ,
+              ) ,
+            ) ,
+            Padding(
+              padding: const EdgeInsets.all (8.0) ,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, "No!");
+                },
+                child: Text('No!'),
+              ) ,
+            )
+          ],
+        ) ,
+      ) ,
+    ) ;
+  }
 }
